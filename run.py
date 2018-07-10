0b641584a6e597ac2e8004caab40578b819ed37b
helloben <jbbailey2@crimson.ua.edu>
2018-07-10 14:14:41 -0400

    Modified run.py with port config

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from flaskr import create_app
+import os
 
 app = create_app()
-app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+# app.run(debug=True)
