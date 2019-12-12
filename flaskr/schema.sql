7c274229d9b6695f103729ce4447d6c0d1235fa0
helloben <jbbailey2@crimson.ua.edu>
2019-12-11 18:03:44 -0600

    schema.sql has queries to create DB

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,24 +1,18 @@
-DROP TABLE IF EXISTS "user";
-DROP TABLE IF EXISTS "log";
+USE `ez-log-db`;
 
-CREATE TABLE "user"
+CREATE TABLE user
 (
-    -- id INTEGER PRIMARY KEY AUTOINCREMENT,
-    -- id INTEGER PRIMARY KEY,
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE NOT NULL,
+    username TEXT NOT NULL,
     password TEXT NOT NULL
 );
 
-CREATE TABLE "log"
+CREATE TABLE log
 (
-    -- id INTEGER PRIMARY KEY AUTOINCREMENT,
-    -- id INTEGER PRIMARY KEY,
     id SERIAL PRIMARY KEY,
     author_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    rating INTEGER NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES "user" (id)
+    rating INTEGER NOT NULL
 );
